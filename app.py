b3ab8930a5bdf6dd2e6e3a142fc7e9e8b1730169
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-01-23 17:51:31 +0000

    Implemented throughput summary table functionality

--- app.py
+++ app.py
@@ -14,8 +14,14 @@ app.layout = dbc.Container([
         id="testdir-input"
     ),
     dcc.Dropdown([], multi=True, id="test-dropdown", placeholder="Select one or more tests"),
-    html.Div(id="latency-summary-output", style={"maxWidth": "100vw", "overflowX": "scroll"}),
-    html.Div(id="throughput-summary-output")
+    html.Div([
+        html.H1("Latency Summary Stats"),    
+        html.Div(id="latency-summary-output", style={"maxWidth": "100vw", "overflowX": "scroll"}),
+    ]),
+    html.Div([
+        html.H1("Throughput Summary Stats"),    
+        html.Div(id="throughput-summary-output", style={"maxWidth": "100vw", "overflowX": "scroll"}),
+    ]),
 ], style={"marginTop": "2vh"}, fluid=True)
 
 @app.callback(
@@ -41,7 +47,7 @@ def populate_summary(tests):
         return "", ""
     
     lat_summaries = []
-    stat_headers = []
+    tp_summaries = []
     for test in tests:
         rundir = os.path.join(test, "run_1")
         
@@ -55,6 +61,8 @@ def populate_summary(tests):
         
         if "pub_0.csv" not in csv_files:
             return "", ""
+        
+        sub_files = [file for file in csv_files if "sub" in file]
         
         pubdir = os.path.join(rundir, "pub_0.csv")
         
@@ -97,8 +105,50 @@ def populate_summary(tests):
         
         lat_summaries.append(lat_summary_stats)
         
-        for key, value in lat_summary_stats.items():
-            stat_headers.append(key)
+        sub_files = [os.path.join(rundir, file) for file in sub_files]
+        
+        sub_dfs = []
+        
+        for file in sub_files:
+            df = pd.read_csv(file, on_bad_lines="skip", skiprows=2, skipfooter=3, engine="python")
+            sub_dfs.append(df)
+            
+        sub_df = pd.concat(sub_dfs, axis=0, ignore_index=True)
+        
+        tp_head = [x for x in sub_df.columns if "mbps" in x.lower()][0]
+        
+        tp_df = sub_df[tp_head]
+        
+        count = len(tp_df.index)
+        mean = tp_df.mean()
+        median = tp_df.median()
+        variance = tp_df.var()
+        std = tp_df.std()
+        skew = tp_df.skew()
+        df_range = tp_df.max() - tp_df.min()
+        lower_quartile = tp_df.quantile(.25)
+        upper_quartile = tp_df.quantile(.75)
+        interquartile_range = upper_quartile - lower_quartile
+        df_min = tp_df.min()
+        df_max = tp_df.max()
+        
+        tp_summary_stats =  {
+            "test": test, 
+            "count": count, 
+            "mean": mean, 
+            "median": median, 
+            "variance": variance, 
+            "std": std, 
+            "skew": skew, 
+            "range": df_range, 
+            "lower_quartile": lower_quartile, 
+            "upper_quartile": upper_quartile, 
+            "interquartile_range": interquartile_range, 
+            "min": df_min,
+            "max": df_max
+        }
+        
+        tp_summaries.append(tp_summary_stats)
         
     lat_summary_table = dbc.Table([
         html.Thead(
@@ -122,7 +172,29 @@ def populate_summary(tests):
         ])
     ], bordered=True, hover=True)
     
-    return lat_summary_table, ""
+    tp_summary_table = dbc.Table([
+        html.Thead(
+            html.Tr(
+                [html.Th("Stat")] + [html.Th( os.path.basename(tp_summary["test"]) ) for tp_summary in tp_summaries]
+            )
+        ),
+        html.Tbody([
+            html.Tr([html.Td("Count")] + [html.Td("{0:,.2f}".format(summary["count"])) for summary in tp_summaries]),
+            html.Tr([html.Td("mean")] + [html.Td("{0:,.2f}".format(summary["mean"])) for summary in tp_summaries]),
+            html.Tr([html.Td("median")] + [html.Td("{0:,.2f}".format(summary["median"])) for summary in tp_summaries]),
+            html.Tr([html.Td("variance")] + [html.Td("{0:,.2f}".format(summary["variance"])) for summary in tp_summaries]),
+            html.Tr([html.Td("std")] + [html.Td("{0:,.2f}".format(summary["std"])) for summary in tp_summaries]),
+            html.Tr([html.Td("skew")] + [html.Td("{0:,.2f}".format(summary["skew"])) for summary in tp_summaries]),
+            html.Tr([html.Td("range")] + [html.Td("{0:,.2f}".format(summary["range"])) for summary in tp_summaries]),
+            html.Tr([html.Td("lower_quartile")] + [html.Td("{0:,.2f}".format(summary["lower_quartile"])) for summary in tp_summaries]),
+            html.Tr([html.Td("upper_quartile")] + [html.Td("{0:,.2f}".format(summary["upper_quartile"])) for summary in tp_summaries]),
+            html.Tr([html.Td("interquartile_range")] + [html.Td("{0:,.2f}".format(summary["interquartile_range"])) for summary in tp_summaries]),
+            html.Tr([html.Td("min")] + [html.Td("{0:,.2f}".format(summary["min"])) for summary in tp_summaries]),
+            html.Tr([html.Td("max")] + [html.Td("{0:,.2f}".format(summary["max"])) for summary in tp_summaries])
+        ])
+    ], bordered=True, hover=True)
+        
+    return lat_summary_table, tp_summary_table
 
 if __name__ == "__main__": 
     app.run_server(debug=True, host="127.0.0.1", port="6745")
