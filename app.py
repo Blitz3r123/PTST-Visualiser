edd5459cde3b11f4a0cf0e4310841bb8ba0bb553
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-01-23 17:38:52 +0000

    Removed old comments

--- app.py
+++ app.py
@@ -107,92 +107,22 @@ def populate_summary(tests):
             )
         ),
         html.Tbody([
-            html.Tr(
-                [html.Td("Count")] + [html.Td("{0:,.2f}".format(summary["count"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("mean")] + [html.Td("{0:,.2f}".format(summary["mean"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("median")] + [html.Td("{0:,.2f}".format(summary["median"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("variance")] + [html.Td("{0:,.2f}".format(summary["variance"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("std")] + [html.Td("{0:,.2f}".format(summary["std"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("skew")] + [html.Td("{0:,.2f}".format(summary["skew"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("range")] + [html.Td("{0:,.2f}".format(summary["range"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("lower_quartile")] + [html.Td("{0:,.2f}".format(summary["lower_quartile"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("upper_quartile")] + [html.Td("{0:,.2f}".format(summary["upper_quartile"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("interquartile_range")] + [html.Td("{0:,.2f}".format(summary["interquartile_range"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("min")] + [html.Td("{0:,.2f}".format(summary["min"])) for summary in lat_summaries]
-            ),
-            html.Tr(
-                [html.Td("max")] + [html.Td("{0:,.2f}".format(summary["max"])) for summary in lat_summaries]
-            )
+            html.Tr([html.Td("Count")] + [html.Td("{0:,.2f}".format(summary["count"])) for summary in lat_summaries]),
+            html.Tr([html.Td("mean")] + [html.Td("{0:,.2f}".format(summary["mean"])) for summary in lat_summaries]),
+            html.Tr([html.Td("median")] + [html.Td("{0:,.2f}".format(summary["median"])) for summary in lat_summaries]),
+            html.Tr([html.Td("variance")] + [html.Td("{0:,.2f}".format(summary["variance"])) for summary in lat_summaries]),
+            html.Tr([html.Td("std")] + [html.Td("{0:,.2f}".format(summary["std"])) for summary in lat_summaries]),
+            html.Tr([html.Td("skew")] + [html.Td("{0:,.2f}".format(summary["skew"])) for summary in lat_summaries]),
+            html.Tr([html.Td("range")] + [html.Td("{0:,.2f}".format(summary["range"])) for summary in lat_summaries]),
+            html.Tr([html.Td("lower_quartile")] + [html.Td("{0:,.2f}".format(summary["lower_quartile"])) for summary in lat_summaries]),
+            html.Tr([html.Td("upper_quartile")] + [html.Td("{0:,.2f}".format(summary["upper_quartile"])) for summary in lat_summaries]),
+            html.Tr([html.Td("interquartile_range")] + [html.Td("{0:,.2f}".format(summary["interquartile_range"])) for summary in lat_summaries]),
+            html.Tr([html.Td("min")] + [html.Td("{0:,.2f}".format(summary["min"])) for summary in lat_summaries]),
+            html.Tr([html.Td("max")] + [html.Td("{0:,.2f}".format(summary["max"])) for summary in lat_summaries])
         ])
     ], bordered=True, hover=True)
     
     return lat_summary_table, ""
-    
-    # if test:
-    #     if os.path.exists(os.path.join(test, "run_1")):
-    #         csv_files = [file for file in os.listdir(os.path.join(test, "run_1")) if ".csv" in file]
-        
-    #         if len(csv_files) == 0:
-    #             lat_summary = f"{test} has no csv files."
-    #             tp_summary = f"{test} has no csv files."
-        
-    #         if "pub_0.csv" not in csv_files:
-    #             raise Exception(f"{test} has no pub_0.csv file.")
-            
-    #         pub = os.path.join(test, "run_1", "pub_0.csv")
-            
-    #         lat_df = pd.read_csv(pub, on_bad_lines="skip", skiprows=2)
-            
-    #         lat_header = [col for col in lat_df.columns if "latency" in col.lower()][0]
-            
-    #         lat_df = lat_df[lat_header]
-            
-    #         lat_summary = dcc.Markdown(lat_df.describe().to_frame().to_markdown())
-            
-    #         sub_files = [file for file in csv_files if "sub" in file]
-            
-    #         if len(sub_files) == 0:
-    #             tp_summary = f"No sub csv files found for {test}."
-    #         else:
-    #             tp_children = []
-    #             for file in sub_files:
-    #                 file = os.path.join(test, "run_1", file)
-    #                 df = pd.read_csv(file, on_bad_lines="skip", skiprows=2, skipfooter=3)
-    #                 tp_header = [col for col in df.columns if "mbps" in col.lower()][0]
-                    
-    #                 tp_df = df[tp_header].to_frame()
-                    
-    #                 for col in tp_df.columns:
-    #                     tp_df[col] = tp_df[col].astype(float)
-                    
-    #                 tp_df = tp_df.rename(columns={ tp_header: os.path.basename(file) })
-                    
-    #                 tp_children.append(html.Td(dcc.Markdown(tp_df.describe().to_markdown())))
-                    
-    #             tp_summary = dbc.Table(html.Tr(tp_children), bordered=True, hover=True)
-            
-    #     else:
-    #         lat_summary, tp_summary = f"run_1 folder not found for {test}."
 
 if __name__ == "__main__": 
     app.run_server(debug=True, host="127.0.0.1", port="6745")
