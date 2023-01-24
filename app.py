cd028abf6efb65bdff9c1628be6160721becca52
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-01-24 15:30:02 +0000

    Refactored code for summary tables and log timeline

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 
 from pprint import pprint
+from functions import *
 from dash import Dash, html, dcc, Output, Input
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -56,131 +57,17 @@ def populate_summary(tests):
     tp_summaries = []
     log_timelines = []
     for test in tests:
-        rundir = os.path.join(test, "run_1")
-        
-        if not os.path.exists(rundir):
-            return "", "", ""
-        
-        csv_files = [file for file in os.listdir(rundir) if ".csv" in file]
-        
-        if len(csv_files) == 0:
-            return "", "", ""
-        
-        if "pub_0.csv" not in csv_files:
-            return "", "", ""
-        
-        sub_files = [file for file in csv_files if "sub" in file]
-        
-        pubdir = os.path.join(rundir, "pub_0.csv")
-        
-        lat_df = pd.read_csv(pubdir, on_bad_lines="skip", skiprows=2, skipfooter=3, engine="python")
-        
-        try:
-            lat_head = [col for col in lat_df.columns if "latency" in col.lower()][0]
-            lat_df = lat_df[lat_head]
-        except Exception as e:
-            return "", "", ""
-        
-        count = len(lat_df.index)
-        mean = lat_df.mean()
-        median = lat_df.median()
-        variance = lat_df.var()
-        std = lat_df.std()
-        skew = lat_df.skew()
-        df_range = lat_df.max() - lat_df.min()
-        lower_quartile = lat_df.quantile(.25)
-        upper_quartile = lat_df.quantile(.75)
-        interquartile_range = upper_quartile - lower_quartile
-        df_min = lat_df.min()
-        df_max = lat_df.max()
-        
-        lat_summary_stats =  {
-            "test": test, 
-            "count": count, 
-            "mean": mean, 
-            "median": median, 
-            "variance": variance, 
-            "std": std, 
-            "skew": skew, 
-            "range": df_range, 
-            "lower_quartile": lower_quartile, 
-            "upper_quartile": upper_quartile, 
-            "interquartile_range": interquartile_range, 
-            "min": df_min,
-            "max": df_max
-        }
-        
+        lat_df = get_lat_df(test)
+        lat_summary_stats = get_summary_stats("latency", lat_df, test)
         lat_summaries.append(lat_summary_stats)
         
-        sub_files = [os.path.join(rundir, file) for file in sub_files]
-        
-        sub_dfs = []
-        
-        for file in sub_files:
-            df = pd.read_csv(file, on_bad_lines="skip", skiprows=2, skipfooter=3, engine="python")
-            sub_dfs.append(df)
-            
-        sub_df = pd.concat(sub_dfs, axis=0, ignore_index=True)
-        
-        tp_head = [x for x in sub_df.columns if "mbps" in x.lower()][0]
-        
-        tp_df = sub_df[tp_head]
-        
-        count = len(tp_df.index)
-        mean = tp_df.mean()
-        median = tp_df.median()
-        variance = tp_df.var()
-        std = tp_df.std()
-        skew = tp_df.skew()
-        df_range = tp_df.max() - tp_df.min()
-        lower_quartile = tp_df.quantile(.25)
-        upper_quartile = tp_df.quantile(.75)
-        interquartile_range = upper_quartile - lower_quartile
-        df_min = tp_df.min()
-        df_max = tp_df.max()
-        
-        tp_summary_stats =  {
-            "test": test, 
-            "count": count, 
-            "mean": mean, 
-            "median": median, 
-            "variance": variance, 
-            "std": std, 
-            "skew": skew, 
-            "range": df_range, 
-            "lower_quartile": lower_quartile, 
-            "upper_quartile": upper_quartile, 
-            "interquartile_range": interquartile_range, 
-            "min": df_min,
-            "max": df_max
-        }
-        
+        tp_df = get_tp_df(test)
+        tp_summary_stats = get_summary_stats("throughput", tp_df, test)
         tp_summaries.append(tp_summary_stats)
         
-        logdir = os.path.join(rundir, "logs")
-        logs = [os.path.join(logdir, file) for file in os.listdir(logdir)]
-        cpu_logs = [file for file in logs if "_cpu.log" in file]
+        cpu_log_df = get_cpu_log_df(test)
         
-        cpu_logs_data = []
-        
-        for log in cpu_logs:
-            cpu_log_data = {
-                "start": "",
-                "end": "",
-                "vm": os.path.basename(log).replace("_cpu.log", "").replace("csr-dds-", "").replace("app", "vm")
-            }
-            
-            log_df = pd.read_csv(log, skiprows=1, skipfooter=1, engine="python", on_bad_lines="skip", delim_whitespace=True)
-            
-            time_df = log_df.iloc[:,0]
-            cpu_log_data["start"] = "1970-01-01 " + str(time_df.min())
-            cpu_log_data["end"] = "1970-01-01 " + str(time_df.max())
-            
-            cpu_logs_data.append(cpu_log_data)
-            
-        test_df = pd.DataFrame(cpu_logs_data)
-        
-        fig = px.timeline(test_df, x_start="start", x_end="end", y="vm", color="vm")
+        fig = px.timeline(cpu_log_df, x_start="start", x_end="end", y="vm", color="vm")
         fig.update_layout(xaxis=dict(
             title="Log Timestamp",
             tickformat="%H:%M:%S"
--- functions.py
+++ functions.py
@@ -6,46 +6,110 @@ from rich.console import Console
 
 console = Console()
 
-def get_latency_summary(test):
-    csv_files = [file for file in os.listdir(os.path.join(test, "run_1")) if ".csv" in file]
+def get_summary_stats(type, df, test):
+    if "latency" in type or "throughput" in type:
+        count = len(df.index)
+        mean = df.mean()
+        median = df.median()
+        variance = df.var()
+        std = df.std()
+        skew = df.skew()
+        df_range = df.max() - df.min()
+        lower_quartile = df.quantile(.25)
+        upper_quartile = df.quantile(.75)
+        interquartile_range = upper_quartile - lower_quartile
+        df_min = df.min()
+        df_max = df.max()
+        
+        summary_stats =  {
+            "test": test, 
+            "count": count, 
+            "mean": mean, 
+            "median": median, 
+            "variance": variance, 
+            "std": std, 
+            "skew": skew, 
+            "range": df_range, 
+            "lower_quartile": lower_quartile, 
+            "upper_quartile": upper_quartile, 
+            "interquartile_range": interquartile_range, 
+            "min": df_min,
+            "max": df_max
+        }
+        
+        
+    return summary_stats
+
+def get_lat_df(test):
+    rundir = os.path.join(test, "run_1")
+        
+    if not os.path.exists(rundir):
+        return
+    
+    csv_files = [file for file in os.listdir(rundir) if ".csv" in file]
+    
+    if len(csv_files) == 0:
+        return
     
     if "pub_0.csv" not in csv_files:
-        raise Exception(f"{test} has no pub_0.csv file.")
+        return
     
-    pub = os.path.join(test, "run_1", "pub_0.csv")
+    sub_files = [file for file in csv_files if "sub" in file]
     
-    lat_df = pd.read_csv(pub, on_bad_lines="skip", skiprows=2)
+    pubdir = os.path.join(rundir, "pub_0.csv")
     
-    lat_header = [col for col in lat_df.columns if "latency" in col.lower()][0]
+    lat_df = pd.read_csv(pubdir, on_bad_lines="skip", skiprows=2, skipfooter=3, engine="python")
     
-    lat_df = lat_df[lat_header]
+    try:
+        lat_head = [col for col in lat_df.columns if "latency" in col.lower()][0]
+        lat_df = lat_df[lat_head]
+    except Exception as e:
+        return
     
-    count = len(lat_df.index)
-    mean = lat_df.mean()
-    median = lat_df.median()
-    variance = lat_df.var()
-    std = lat_df.std()
-    # cov = lat_df.cov()
-    skew = lat_df.skew()
-    df_range = lat_df.max() - lat_df.min()
-    lower_quartile = lat_df.quantile(.25)
-    upper_quartile = lat_df.quantile(.75)
-    interquartile_range = upper_quartile - lower_quartile
-    df_min = lat_df.min()
-    df_max = lat_df.max()
+    return lat_df
+
+def get_tp_df(test):
+    rundir = os.path.join(test, "run_1")
+    csv_files = [file for file in os.listdir(rundir) if ".csv" in file]
+    sub_files = [file for file in csv_files if "sub" in file]
+    sub_files = [os.path.join(rundir, file) for file in sub_files]
+        
+    sub_dfs = []
     
-    return [
-        {"count": count},
-        {"mean": mean},
-        {"median": median},
-        {"variance": variance},
-        {"std": std},
-        # {"cov": cov},
-        {"skew": skew},
-        {"range": df_range},
-        {"lower_quartile": lower_quartile},
-        {"upper_quartile": upper_quartile},
-        {"interquartile_range": interquartile_range},
-        {"min": df_min},
-        {"max": df_max}
-    ]
+    for file in sub_files:
+        df = pd.read_csv(file, on_bad_lines="skip", skiprows=2, skipfooter=3, engine="python")
+        sub_dfs.append(df)
+        
+    sub_df = pd.concat(sub_dfs, axis=0, ignore_index=True)
+    
+    tp_head = [x for x in sub_df.columns if "mbps" in x.lower()][0]
+    
+    tp_df = sub_df[tp_head]
+    
+    return tp_df
+
+def get_cpu_log_df(test):
+    logdir = os.path.join(test, "run_1", "logs")
+    logs = [os.path.join(logdir, file) for file in os.listdir(logdir)]
+    cpu_logs = [file for file in logs if "_cpu.log" in file]
+    
+    cpu_logs_data = []
+    
+    for log in cpu_logs:
+        cpu_log_data = {
+            "start": "",
+            "end": "",
+            "vm": os.path.basename(log).replace("_cpu.log", "").replace("csr-dds-", "").replace("app", "vm")
+        }
+        
+        log_df = pd.read_csv(log, skiprows=1, skipfooter=1, engine="python", on_bad_lines="skip", delim_whitespace=True)
+        
+        # ? Pick the first column
+        time_df = log_df.iloc[:,0]
+        
+        cpu_log_data["start"] = "1970-01-01 " + str(time_df.min())
+        cpu_log_data["end"] = "1970-01-01 " + str(time_df.max())
+        
+        cpu_logs_data.append(cpu_log_data)
+        
+    return pd.DataFrame(cpu_logs_data)
