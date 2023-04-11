c71fae6e07a48b60f02b31c2dcbb72c929ea15f0
Kaleem Peeroo <kaleem.peeroo@city.ac.uk>
2023-04-11 20:18:03 +0100

    Added comments for skeletal structure of process.py

---
+++ process.py
@@ -0,0 +1,11 @@
+
+"""
+1. Find usable tests.
+2. Copy usable tests over to usable_dir.
+3. Summarise tests in usable_dir.
+"""
+# ? 1. Find usable tests.
+
+# ? 2. Copy usable tests over to usable_dir.
+
+# ? 3. Summarise tests in usable_dir.
